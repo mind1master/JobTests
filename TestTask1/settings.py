24016510e9633e3f99b5c70b1d221cafdf02b822
Anton Kasyanov <mind_master@ukr.net>
2012-01-27 19:58:01 +0200

    ticket:4 moved context processor

--- TestTask1/settings.py
+++ TestTask1/settings.py
@@ -100,7 +100,7 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
                                "django.core.context_processors.media",
                                "django.core.context_processors.static",
                                "django.contrib.messages.context_processors.messages",
-                               "TestTask1.context_processors.settings_processor",)
+                               "TestTask1.main.context_processors.settings_processor",)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
