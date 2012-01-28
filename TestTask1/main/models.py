576cccf2a19f74eb4efd9c669a7c25125565cb67
Anton Kasyanov <mind_master@ukr.net>
2012-01-28 12:12:29 +0200

    ticket:5 image saving

--- TestTask1/main/models.py
+++ TestTask1/main/models.py
@@ -15,4 +15,4 @@ class Person(models.Model):
     skype = models.CharField(max_length=20)
     email = models.EmailField()
     phone = models.CharField(max_length=15)
-    #photo = models.ImageField(width_field=600, height_field=1000, upload_to='/avatar')
+    photo = models.ImageField(upload_to='avatar')
