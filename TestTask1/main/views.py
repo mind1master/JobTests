7ae681a05b87a0365101630df84a9ccc59f66f58
Anton Kasyanov <mind_master@ukr.net>
2012-01-30 23:01:54 +0200

    ticket:6 fixed photo loading

--- TestTask1/main/views.py
+++ TestTask1/main/views.py
@@ -10,24 +10,23 @@ from TestTask1.main.models import Person
 
 @login_required
 def editPerson(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = PersonForm(request.POST, request.FILES) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
+    p = Person.objects.get(pk=1)
+    if request.method == 'POST':
+        form = PersonForm(request.POST, request.FILES) #
+        if form.is_valid():
             np = form.save(commit=False)
+            if (np.photo.name == None):
+                np.photo = p.photo
             np.pk = 1
             np.save()
-            return HttpResponse(simplejson.dumps({'response': '', 'result': 'success'}))
-            #return HttpResponseRedirect('/') # Redirect after POST
+            return HttpResponse(simplejson.dumps({'response': np.photo.url, 'result': 'success'}))
         else:
-        # Заполняем словарь response ошибками формы, ключ - название поля
             response = {}
             for k in form.errors:
-                # Теоретически у поля может быть несколько ошибок...
                 response[k] = form.errors[k][0]
             return HttpResponse(simplejson.dumps({'response': response, 'result': 'error'}))
     else:
-        p = Person.objects.get(pk=1)
         form = PersonForm(instance=p) # An unbound form
 
-    c = {'form': form}
+    c = {'form': form, 'profile': p}
     return render_to_response("edit.html", c, context_instance=RequestContext(request))
