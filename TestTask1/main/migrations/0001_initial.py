00da53d1117c52110aa85e9778c98042f67fb1da
Anton Kasyanov <mind_master@ukr.net>
2012-02-01 00:02:42 +0200

    ticket:4 migrations added

---
+++ TestTask1/main/migrations/0001_initial.py
@@ -0,0 +1,64 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Request'
+        db.create_table('main_request', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('header', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('body', self.gf('django.db.models.fields.CharField')(max_length=300)),
+            ('time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('main', ['Request'])
+
+        # Adding model 'Person'
+        db.create_table('main_person', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=25)),
+            ('surname', self.gf('django.db.models.fields.CharField')(max_length=25)),
+            ('birth_date', self.gf('django.db.models.fields.DateField')()),
+            ('bio', self.gf('django.db.models.fields.TextField')(max_length=300)),
+            ('skype', self.gf('django.db.models.fields.CharField')(max_length=20)),
+            ('email', self.gf('django.db.models.fields.EmailField')(max_length=75)),
+            ('phone', self.gf('django.db.models.fields.CharField')(max_length=15)),
+        ))
+        db.send_create_signal('main', ['Person'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Request'
+        db.delete_table('main_request')
+
+        # Deleting model 'Person'
+        db.delete_table('main_person')
+
+
+    models = {
+        'main.person': {
+            'Meta': {'object_name': 'Person'},
+            'bio': ('django.db.models.fields.TextField', [], {'max_length': '300'}),
+            'birth_date': ('django.db.models.fields.DateField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '15'}),
+            'skype': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '25'})
+        },
+        'main.request': {
+            'Meta': {'object_name': 'Request'},
+            'body': ('django.db.models.fields.CharField', [], {'max_length': '300'}),
+            'header': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['main']
