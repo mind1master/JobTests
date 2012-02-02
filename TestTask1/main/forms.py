b91e5489f86465b1d7d307e681775b9c1db60ab2
Anton Kasyanov <mind_master@ukr.net>
2012-02-02 12:44:24 +0200

    ticket:7 form.as_p out

--- TestTask1/main/forms.py
+++ TestTask1/main/forms.py
@@ -6,6 +6,7 @@ from TestTask1.main.models import Person
 class PersonForm(ModelForm):
     class Meta:
         model = Person
+        fields = ('bio', 'birth_date', 'surname', 'name', 'photo', 'phone', 'email', 'skype',)
 
     def __init__(self, *args, **kwargs):
         super(PersonForm, self).__init__(*args, **kwargs)
