e50e3bf77f7ee3a80e22a83abc2dc82dbd3d4d5c
Anton Kasyanov <mind_master@ukr.net>
2012-02-03 15:28:00 +0200

    ticket:8 caching AttributeError

--- TestTask1/main/templatetags/my_tags.py
+++ TestTask1/main/templatetags/my_tags.py
@@ -5,5 +5,9 @@ register = template.Library()
 
 @register.simple_tag
 def edit_link(obj):
-    url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.module_name), args=[obj.id])
-    return u'<a href="%s">Edit %s</a>' % (url, obj.__str__())
+    try:
+        url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.module_name), args=[obj.id])
+        return u'<a href="%s">Edit %s</a>' % (url, obj.__str__())
+    except AttributeError:
+        return 'Sorry, no admin link for this.'
+
