d7f30fb785c2f351301c7566a43f8c841d27d027
Anton Kasyanov <mind_master@ukr.net>
2012-02-06 20:26:04 +0200

    ticket:9 tests updated

--- TestTask1/main/tests.py
+++ TestTask1/main/tests.py
@@ -9,7 +9,10 @@ from django.test import TestCase
 from django.test.client import Client
 from TestTask1.main.models import Request
 from django.conf import settings
-
+from TestTask1.main.forms import PersonForm
+from TestTask1.main.models import Person
+import os
+import datetime
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -37,7 +40,7 @@ class MainPageTest(TestCase):
 
 
 class AuthTest(TestCase):
-    def test_form(self):
+    def test_auth(self):
         c = Client()
         response = c.get('/login/')
         self.assertEqual(response.status_code, 200)
@@ -45,6 +48,22 @@ class AuthTest(TestCase):
         self.assertEqual(response.status_code, 302)
         response = c.get('/edit/')
         self.assertEqual(response.status_code, 302)
+        self.assertEqual(c.login(username='admin', password='admin'), True)
+        response = c.get('/edit/')
+        self.assertEqual(response.status_code, 200)
+
+
+class FormTest(TestCase):
+    def test_form(self):
+        p = Person.objects.get(pk=1)
+        mdata = {'name': p.name, 'surname': p.surname, 'birth_date': p.birth_date,
+                 'bio': p.bio, 'skype': p.skype, 'email': p.email,
+                 'phone': p.phone, 'photo': p.photo}
+        form = PersonForm(data=mdata)
+        self.assertEqual(form.is_valid(), True)
+        mdata['name'] = None
+        form = PersonForm(data=mdata)
+        self.assertEqual(form.is_valid(), False)
 
 
 class RequestsPageTest(TestCase):
@@ -60,3 +79,28 @@ class ContextTest(TestCase):
         c = Client()
         response = c.get('/')
         self.assertEqual(response.context['settings'], settings)
+
+
+class TagTest(TestCase):
+    def test_tag(self):
+        c = Client()
+        response = c.get('/')
+        self.assertContains(response, '/admin/main/person/1/')
+        self.assertContains(response, 'Sorry, no admin link for this.')
+        self.assertNotContains(response, '/admin/auth/user/1/')
+        c.login(username='admin', password='admin')
+        response = c.get('/')
+        self.assertContains(response, '/admin/auth/user/1/')
+        self.assertNotContains(response, 'Sorry, no admin link for this.')
+
+
+class CommandTest(TestCase):
+    def test_list_models(self):
+        self.assertTrue(os.path.isfile(settings.PROJECT_PATH + '/list_models'))
+        a = os.system('cd ' + settings.PROJECT_PATH + '/ && ./list_models')
+        self.assertEqual(a, 0)
+        self.assertTrue(
+            os.path.isfile(settings.PROJECT_PATH + '/' + datetime.date.today().strftime('%m_%d_%Y') + '.dat'))
+        os.remove(settings.PROJECT_PATH + '/' + datetime.date.today().strftime('%m_%d_%Y') + '.dat')
+
+
