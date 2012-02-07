eea979c5d1ccd48bf9c9e96f54a188484067e134
Anton Kasyanov <mind_master@ukr.net>
2012-02-08 01:14:37 +0200

    ticket:9 added exception catch to command

--- TestTask1/main/management/commands/list_models.py
+++ TestTask1/main/management/commands/list_models.py
@@ -1,11 +1,14 @@
 from django.core.management.base import NoArgsCommand
-from django.db import models
+from django.db import models, utils
 
 class Command(NoArgsCommand):
     help = 'Lists all models and object count'
 
     def handle_noargs(self, **options):
-        models_list = models.get_models()
-        for m in models_list:
-            self.stdout.write('%s - %d objects \n' % (m, m.objects.count()))
-            self.stderr.write('error: %s - %d objects \n' % (m, m.objects.count()))
+        try:
+            models_list = models.get_models()
+            for m in models_list:
+                self.stdout.write('%s - %d objects \n' % (m, m.objects.count()))
+                self.stderr.write('error: %s - %d objects \n' % (m, m.objects.count()))
+        except utils.DatabaseError:
+            self.stderr.write('error: Can\' get models list. Probably you didn\'t perform any syncdb.')
