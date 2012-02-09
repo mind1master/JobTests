db30ff67d4096e8a2295bd50b6bf2a240372a11c
Anton Kasyanov <mind_master@ukr.net>
2012-02-09 22:49:38 +0200

    ticket:13 added fixtures check using raw

--- TestTask1/main/models.py
+++ TestTask1/main/models.py
@@ -33,10 +33,10 @@ class SignalInfo(models.Model):
         return '{0} at {1}'.format(self.body, self.time)
 
 
-def edit_callback(sender, created, raw, instance, **kwargs):
+def edit_callback(sender, created, instance, **kwargs):
     if (sender == SignalInfo):
         return
-    if (raw):
+    if (kwargs.get('raw', True)):
         return
     if (created):
         action = 'created'
